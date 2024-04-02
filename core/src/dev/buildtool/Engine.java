0f0888667a1b6e4082818955d4f3539f96911c48
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-02 16:42:00 +0300

    Add jump distance

--- core/src/dev/buildtool/Engine.java
+++ core/src/dev/buildtool/Engine.java
@@ -1,14 +1,16 @@
 package dev.buildtool;
 
 public class Engine {
-    public static final Engine BASIC=new Engine("basic",5,60);
-    public float maxSpeed;
-    public float steering;
-    public String type;
+    public static final Engine BASIC=new Engine("basic",5,60, 400);
+    public final float maxSpeed;
+    public final float steering;
+    public final String type;
+    public final int jumpDistance;
 
-    public Engine(String type, float maxSpeed, float steering) {
+    public Engine(String type, float maxSpeed, float steering, int jumpDistance) {
         this.maxSpeed = maxSpeed;
         this.type=type;
         this.steering = steering;
+        this.jumpDistance=jumpDistance;
     }
 }
