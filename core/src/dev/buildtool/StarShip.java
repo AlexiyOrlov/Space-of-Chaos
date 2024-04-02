ef0b2a50b590d3b393e5c60f5066460eaa231b1e
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-02 20:55:56 +0300

    --

--- core/src/dev/buildtool/StarShip.java
+++ core/src/dev/buildtool/StarShip.java
@@ -30,7 +30,7 @@ public class StarShip {
     private final Array<Projectile> projectiles;
     private float fireDelay;
     public Inventory inventory;
-    private Circle area;
+    private final Circle area;
     public int money=1000;
     public StarShip(float x, float y, float rotation,Texture texture,StarSystem currentStarSystem) {
         this.x = x;
