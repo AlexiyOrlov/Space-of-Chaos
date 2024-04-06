7c9174c0884898e4e7f787844f8b52235fe5a880
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-06 20:09:43 +0300

    Mark 2 engine

--- core/src/dev/buildtool/Engine.java
+++ core/src/dev/buildtool/Engine.java
@@ -3,6 +3,7 @@ package dev.buildtool;
 public class Engine {
     public static final Engine BASIC=new Engine("basic",5,60, 400, 0.02f);
     public static final Engine SLOW=new Engine("basic",2.5f,60, 400, 0.15f);
+    public static final Engine MARK2=new Engine("mark2",6,70,400,0.03f);
     public static final Engine ENGINE_3=new Engine("mark3",7,80,400, 0.03f);
     public final float maxSpeed;
     public final float steering;
