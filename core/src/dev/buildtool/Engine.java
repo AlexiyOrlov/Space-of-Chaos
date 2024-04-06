fe96c031edf918f2ca25996c41c373f9e6344a08
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-06 20:30:20 +0300

    Names

--- core/src/dev/buildtool/Engine.java
+++ core/src/dev/buildtool/Engine.java
@@ -5,8 +5,8 @@ import com.badlogic.gdx.graphics.Texture;
 public class Engine extends Item{
     public static final Engine BASIC=new Engine("basic",5,60, 400, 0.02f,SpaceGame.INSTANCE.engine1Texture,3000);
     public static final Engine SLOW=new Engine("basic",2.5f,60, 400, 0.15f,null,0);
-    public static final Engine MARK2=new Engine("mark2",6,70,400,0.03f,SpaceGame.INSTANCE.engine3Texture,35000);
-    public static final Engine ENGINE_3=new Engine("mark3",7,80,400, 0.03f,SpaceGame.INSTANCE.engine2Texture,70000);
+    public static final Engine MARK2=new Engine("Engine mark2",6,70,400,0.03f,SpaceGame.INSTANCE.engine3Texture,35000);
+    public static final Engine ENGINE_3=new Engine("Engine mark3",7,80,400, 0.03f,SpaceGame.INSTANCE.engine2Texture,70000);
     public final float maxSpeed;
     public final float steering;
     public final String type;
