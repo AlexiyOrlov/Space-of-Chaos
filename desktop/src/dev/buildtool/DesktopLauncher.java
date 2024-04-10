432df7e59ec6da1f6ef6d0306a3d1e8b7b98843e
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-10 20:37:32 +0300

    Came up with game title

--- desktop/src/dev/buildtool/DesktopLauncher.java
+++ desktop/src/dev/buildtool/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Third Game");
+		config.setTitle("Space of chaos");
 		config.setMaximized(true);
 		new Lwjgl3Application(new SpaceGame(), config);
 	}
