aac4fee9bdc2b2481fb5c4eda23481ca0c898891
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-11 00:34:30 +0300

    Change basic gun's texture

--- core/src/dev/buildtool/SpaceGame.java
+++ core/src/dev/buildtool/SpaceGame.java
@@ -32,7 +32,8 @@ public class SpaceGame extends Game {
 		targetTexture,skyTexture, tradingHull1Texture,starGateTexture,inhabitedPlanetIcon,uninhabitedPlanetIcon,
 		battleHull2,pelletTexture,stargateIcon,battleHull3,pirateHull1,engine1Texture,engine2Texture,engine3Texture,shipIcon,
 		ship2icon,slotTexture3,thrusters1Texture,thrusters2Texture,gunTexture,shotgunTexture,cashTexture,drone2Texture,
-		containerTexture,shipIcon3,blackHullTexture,blackHull2Texture,tradingHull2Texture,pirateHull2Texture,pirateHull3Texture;
+		containerTexture,shipIcon3,blackHullTexture,blackHull2Texture,tradingHull2Texture,pirateHull2Texture,pirateHull3Texture,
+		basicGunTexture;
 	public static SpaceGame INSTANCE;
 	public Skin skin;
 	ShapeRenderer shapeRenderer,uiShapeRenderer;
@@ -112,6 +113,7 @@ public class SpaceGame extends Game {
 		assetManager.load("trading hull2.png",Texture.class);
 		assetManager.load("pirate hull2.png", Texture.class);
 		assetManager.load("pirate hull3.png", Texture.class);
+		assetManager.load("basic gun.png", Texture.class);
 		assetManager.finishLoading();
 
 		alcoholTexture=assetManager.get("alcohol.png");
@@ -156,6 +158,7 @@ public class SpaceGame extends Game {
 		blackHullTexture=assetManager.get("hull2.png");
 		blackHull2Texture=assetManager.get("bumblebee hull.png");
 		pirateHull3Texture=assetManager.get("pirate hull3.png");
+		basicGunTexture=assetManager.get("basic gun.png");
 
 		starTextures=new ArrayList<>(3);
 		starTextures.add(assetManager.get("star.png"));
