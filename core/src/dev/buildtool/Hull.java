ceb3a8fe774b20cb8fe2e73aeaea31f0b602f261
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-11 10:10:35 +0300

    -

--- core/src/dev/buildtool/Hull.java
+++ core/src/dev/buildtool/Hull.java
@@ -9,7 +9,7 @@ public class Hull extends Item{
     public static final Hull BASIC=new Hull(100, SpaceGame.INSTANCE.redStarshipTexture ,100,"Basic hull",3000);
     public static final Hull HORNET=new Hull(150,SpaceGame.INSTANCE.blackHullTexture, 160,"Hornet",40000);
     public static final Hull BUMBLEBEE=new Hull(200,SpaceGame.INSTANCE.blackHull2Texture, 200,"Bumblebee",70000);
-    private static final Hull TRADING1=new Hull(250, SpaceGame.INSTANCE.tradingHull1Texture,200,"Trading hull",50000);
+    private static final Hull TRADING1=new Hull(250, SpaceGame.INSTANCE.tradingHull1Texture,200,"Trading hull 1",50000);
     private static final Hull TRADING2=new Hull(300,SpaceGame.INSTANCE.tradingHull2Texture, 240,"Trading hull 2",60000);
     private static final Hull BATTLE3=new Hull(250,SpaceGame.INSTANCE.battleHull3, 400,"Battle hull 3",50000);
     private static final Hull BATTLE1=new Hull(250,SpaceGame.INSTANCE.battleHull3Texture,450,"Battle hull 1",60000);
