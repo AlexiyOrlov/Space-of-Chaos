0d13030777cfbeb7eabcbfe4ab2ed13d487a8baf
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-11 09:01:16 +0300

    Increase speed

--- core/src/dev/buildtool/weapons/WeaponRegistry.java
+++ core/src/dev/buildtool/weapons/WeaponRegistry.java
@@ -9,7 +9,7 @@ import dev.buildtool.weapons.Weapon;
 
 public class WeaponRegistry {
     public static final TreeMap<String, Weapon> WEAPONS=new TreeMap<>();
-    public static final Weapon GUN=new Gun(10,1,10, SpaceGame.INSTANCE.basicProjectile, "Basic gun");
+    public static final Weapon GUN=new Gun(10,1,15, SpaceGame.INSTANCE.basicProjectile, "Basic gun");
     public static final Weapon SHOTGUN=new Shotgun(5,2,30,SpaceGame.INSTANCE.pelletTexture, "Shotgun");
 
     public static final Weapon AI_GUN1=new Gun(15,1,20,SpaceGame.INSTANCE.basicProjectile, "Basic AI gun");
