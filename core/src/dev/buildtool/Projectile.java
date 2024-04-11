488975516d1e1aa3d939488fd7b4a6eb7c0d9d70
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-11 20:17:40 +0300

    =

--- core/src/dev/buildtool/Projectile.java
+++ core/src/dev/buildtool/Projectile.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Projectile {
