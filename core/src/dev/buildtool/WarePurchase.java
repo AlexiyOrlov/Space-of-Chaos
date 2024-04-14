1093f13487f7c9ff893e9117975330659b2c4d3a
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-14 18:56:47 +0300

    Implement

--- core/src/dev/buildtool/WarePurchase.java
+++ core/src/dev/buildtool/WarePurchase.java
@@ -1,15 +1,35 @@
 package dev.buildtool;
 
-public class WarePurchase {
-    final Ware ware;
-    final int amountBought;
-    final int pricePerUnit;
-    final int moneySpent;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WarePurchase implements SaveData {
+     Ware ware;
+     int amountBought;
+     int pricePerUnit;
+     int moneySpent;
 
     public WarePurchase(Ware ware, int amountBought, int pricePerUnit, int moneySpent) {
         this.ware = ware;
         this.amountBought = amountBought;
         this.pricePerUnit = pricePerUnit;
         this.moneySpent = moneySpent;
+    }
+
+    @Override
+    public Map<String, Object> getData() {
+        HashMap<String,Object> data=new HashMap<>();
+        data.put("ware",ware.name);
+        data.put("amount bought",amountBought);
+        data.put("money spent",moneySpent);
+        data.put("price per unit",pricePerUnit);
+    }
+
+    @Override
+    public void load(Map<String, Object> data) {
+        ware= (Ware) Item.REGISTRY.get((String)data.get("ware"));
+        amountBought= (int) data.get("amount bought");
+        moneySpent= (int) data.get("money spent");
+        pricePerUnit= (int) data.get("price per unit");
     }
 }
