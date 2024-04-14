a4ba271d0940ebd0ff2ea07ebb9bbbaefa845139
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-14 21:52:38 +0300

    Fix

--- core/src/dev/buildtool/projectiles/Missile.java
+++ core/src/dev/buildtool/projectiles/Missile.java
@@ -6,11 +6,14 @@ import com.badlogic.gdx.math.MathUtils;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
+import dev.buildtool.OneShotAnimation;
 import dev.buildtool.Functions;
 import dev.buildtool.Ship;
+import dev.buildtool.SpaceOfChaos;
 import dev.buildtool.StarSystem;
 
-public class Missile extends Projectile{
+public class Missile extends Projectile implements DestructibleProjectile{
+    private int integrity=30;
     public Missile(Texture texture, int damage, float x, float y, float rotationDegrees, int speed, Ship shooter, Ship target, Predicate<Ship> shipPredicate, StarSystem starSystem) {
         super(texture, damage, x, y, rotationDegrees, speed, shooter, target, shipPredicate,starSystem);
     }
@@ -26,5 +29,23 @@ public class Missile extends Projectile{
         area.set(x,y,texture.getWidth()/2);
         if(target!=null && (target.getCurrentSystem()!=starSystem || target.isLanded() || target.getIntegrity()<=0))
             target=null;
+    }
+
+    @Override
+    public void onDestroyed(StarSystem starSystem) {
+        OneShotAnimation oneShotAnimation =new OneShotAnimation(SpaceOfChaos.INSTANCE.explosionAnimation, x,y);
+        starSystem.animations.add(oneShotAnimation);
+        if(SpaceOfChaos.INSTANCE.playerShip!=null && starSystem==SpaceOfChaos.INSTANCE.playerShip.currentStarSystem)
+            SpaceOfChaos.INSTANCE.explosionSound.play(0.2f);
+    }
+
+    @Override
+    public int getIntegrity() {
+        return integrity;
+    }
+
+    @Override
+    public void damage(int damage) {
+        integrity-=damage;
     }
 }
