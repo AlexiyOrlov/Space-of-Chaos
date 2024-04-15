52d24e55e257ebca12ee367f9b1ddee23479e767
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-15 16:38:45 +0300

    Increase speed

--- core/src/dev/buildtool/weapons/WeaponRegistry.java
+++ core/src/dev/buildtool/weapons/WeaponRegistry.java
@@ -7,7 +7,7 @@ import dev.buildtool.SpaceOfChaos;
 
 public class WeaponRegistry {
     public static final TreeMap<String, Weapon> WEAPONS=new TreeMap<>();
-    public static final Weapon GUN=new Gun(10,1,15, SpaceOfChaos.INSTANCE.basicProjectile, "Basic gun", SpaceOfChaos.INSTANCE.laserShotSound);
+    public static final Weapon GUN=new Gun(10,1,20, SpaceOfChaos.INSTANCE.basicProjectile, "Basic gun", SpaceOfChaos.INSTANCE.laserShotSound);
     public static final Weapon SHOTGUN=new Shotgun(5,2,30, SpaceOfChaos.INSTANCE.pelletTexture, "Shotgun",75000, SpaceOfChaos.INSTANCE.shotGunSound);
     public static final Weapon MACHINE_GUN=new MachineGun(5,0.25f,15, SpaceOfChaos.INSTANCE.pelletTexture,"Machine gun", SpaceOfChaos.INSTANCE.machineGunTexture, 100000, SpaceOfChaos.INSTANCE.machineGunSound);
     public static final Weapon AI_GUN1=new Gun(15,1,20, SpaceOfChaos.INSTANCE.basicProjectile, "Basic AI gun",null);
