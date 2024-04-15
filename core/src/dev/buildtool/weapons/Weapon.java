e91a27bb17338f4b5422227245714672fc120aba
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-15 15:35:43 +0300

    Don't play sound when player is landed

--- core/src/dev/buildtool/weapons/Weapon.java
+++ core/src/dev/buildtool/weapons/Weapon.java
@@ -31,15 +31,14 @@ public abstract class Weapon extends Item {
 
     public Projectile[] shoot(float originX, float originY, float rotation, Ship shooter, Ship target, StarSystem starSystem)
     {
-        if(sound!=null && SpaceOfChaos.INSTANCE.playerShip!=null && shooter.getCurrentSystem()== SpaceOfChaos.INSTANCE.playerShip.getCurrentSystem())
+        if(sound!=null && SpaceOfChaos.INSTANCE.playerShip!=null && !SpaceOfChaos.INSTANCE.playerShip.isLanded() && shooter.getCurrentSystem()== SpaceOfChaos.INSTANCE.playerShip.getCurrentSystem())
             sound.play(0.1f);
         return createProjectiles(originX, originY, rotation, shooter, target,ship -> true, starSystem);
     }
 
     public Projectile[] shoot(float originX, float originY, float rotation, Ship shooter, Ship target, Predicate<Ship> shipPredicate, StarSystem starSystem)
     {
-        if(sound!=null && SpaceOfChaos.INSTANCE.playerShip!=null && shooter.getCurrentSystem()== SpaceOfChaos.INSTANCE.playerShip.getCurrentSystem())
-            sound.play(0.1f);
+        shoot(originX, originY, rotation, shooter, target, starSystem);
         return createProjectiles(originX, originY, rotation, shooter, target,shipPredicate, starSystem);
     }
     public abstract Projectile[] createProjectiles(float originX, float originY, float rotation, Ship shooter, Ship target, Predicate<Ship> shipPredicate, StarSystem starSystem);
