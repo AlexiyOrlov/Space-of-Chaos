3df0521d01284fc6253d8c2944e65ac7764d9b22
AlexiyOrlov <alexiy.ov@gmail.com>
2024-04-17 18:21:10 +0300

    License costs

--- core/src/dev/buildtool/Ware.java
+++ core/src/dev/buildtool/Ware.java
@@ -8,22 +8,24 @@ import java.util.HashMap;
 public class Ware extends Item {
     public static ArrayList<Ware> WARES=new ArrayList<>();
     public static HashMap<Ware,Integer> BASE_PRICES =new HashMap<>();
-    public static Ware MEDICATIONS=new Ware(900,true,"Medications", SpaceOfChaos.INSTANCE.medicineTexture);
+    public static Ware MEDICATIONS=new Ware(900,true,"Medications", SpaceOfChaos.INSTANCE.medicineTexture,250000);
     public static Ware FOOD=new Ware(900,false,"Food", SpaceOfChaos.INSTANCE.foodTexture);
     public static Ware FURNITURE=new Ware(450,false,"Furniture", SpaceOfChaos.INSTANCE.furnitureTexture);
     public static Ware WATER=new Ware(900,false,"Water", SpaceOfChaos.INSTANCE.waterTexture);
     public static Ware ELECTRONICS=new Ware(700,false,"Electronics", SpaceOfChaos.INSTANCE.electronicsTexture);
     public static Ware CLOTHES=new Ware(800,false,"Clothes", SpaceOfChaos.INSTANCE.clothesTexture);
     public static Ware JEWELLERY=new Ware(900,false,"Jewellery", SpaceOfChaos.INSTANCE.jewelleryTexture);
-    public static Ware FIREARMS=new Ware(600,true,"Firearms", SpaceOfChaos.INSTANCE.firearmsTexture);
+    public static Ware FIREARMS=new Ware(600,true,"Firearms", SpaceOfChaos.INSTANCE.firearmsTexture,400000);
     public static Ware CAR_PARTS=new Ware(300,false,"Car parts", SpaceOfChaos.INSTANCE.carPartsTexture);
-    public static Ware TOOLS=new Ware(600,false,"Tools", SpaceOfChaos.INSTANCE.toolsTexture);
+    public static Ware TOOLS=new Ware(600,false,"Tools", SpaceOfChaos.INSTANCE.toolsTexture,100000);
     public static Ware ALCOHOL=new Ware(900,true,"Alcohol", SpaceOfChaos.INSTANCE.alcoholTexture);
     public static Ware IRON_ORE=new Ware(650,false,"Iron ore", SpaceOfChaos.INSTANCE.ironOreTexture);
     public static Ware COPPER_ORE=new Ware(650,false,"Copper ore", SpaceOfChaos.INSTANCE.copperOreTexture);
     public static final int MAXIMUM_WARE_AMOUNT=1000;
     public static final HashMap<Ware,Float> MANUFACTURING_SPEED=new HashMap<>();
     public boolean needsLicense;
+    public int licenseCost;
+
     static {
         WARES.add(TOOLS);
         WARES.add(CAR_PARTS);
@@ -59,5 +61,11 @@ public class Ware extends Item {
     public Ware(int maxSize, boolean needsLicenseToTrade, String name, Texture texture) {
         super(maxSize,name,texture, -1);
         needsLicense=needsLicenseToTrade;
+    }
+
+    public Ware(int maxSize, boolean needsLicense,String name, Texture texture,  int licenseCost) {
+        this(maxSize, needsLicense, name, texture);
+        this.needsLicense = needsLicense;
+        this.licenseCost = licenseCost;
     }
 }
