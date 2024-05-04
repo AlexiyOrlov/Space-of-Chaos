f5d01989499142f1269f7a259546e6025c7daabf
AlexiyOrlov <alexiy.ov@gmail.com>
2024-05-04 12:25:08 +0300

    Decrease delay

--- core/src/dev/buildtool/weapons/WeaponRegistry.java
+++ core/src/dev/buildtool/weapons/WeaponRegistry.java
@@ -12,7 +12,7 @@ public class WeaponRegistry {
     public static final Weapon MACHINE_GUN=new MachineGun(5,0.25f,15, SpaceOfChaos.INSTANCE.pelletTexture,"Machine gun", SpaceOfChaos.INSTANCE.machineGunTexture, 100000, SpaceOfChaos.INSTANCE.shot2sound);
     public static final Weapon AI_GUN1=new Gun(15,1,20, SpaceOfChaos.INSTANCE.basicProjectile, "Basic AI gun",null);
     public static final Weapon CLUSTER_GUN=new ClusterGun(20,1.5f,20, SpaceOfChaos.INSTANCE.redProjectileTexture, "Cluster gun", SpaceOfChaos.INSTANCE.clusterGunTexture, 310000, SpaceOfChaos.INSTANCE.blasterSound);
-    public static final Weapon MISSILE_LAUNCHER=new MissileLauncher(50,6,10,SpaceOfChaos.INSTANCE.missileTexture,"Missile launcher",SpaceOfChaos.INSTANCE.missileLauncherTexture, 200000,null);
+    public static final Weapon MISSILE_LAUNCHER=new MissileLauncher(50,3,10,SpaceOfChaos.INSTANCE.missileTexture,"Missile launcher",SpaceOfChaos.INSTANCE.missileLauncherTexture, 200000,null);
     public static final Weapon GATLING_GUN=new MachineGun(4,0.1f,15,SpaceOfChaos.INSTANCE.pelletTexture, "Gatling gun",SpaceOfChaos.INSTANCE.gatlingGunTexture, 300000,SpaceOfChaos.INSTANCE.machineGunSound);
     public static final Weapon TRISHOT=new TriShot(14,2,20,SpaceOfChaos.INSTANCE.redProjectileTexture, "Trishot",SpaceOfChaos.INSTANCE.triShotTexture, 80000,SpaceOfChaos.INSTANCE.drrrSound);
     public static final ArrayList<Weapon> TIER1_WEAPONS=new ArrayList<>();
